bf5336279e1c245a013097c5361bd203fd9276de
asier <apenao24@fpsanturtzilh.eus>
2026-02-12 08:46:51 +0100

    Últimos Cambios para AWS

--- ile-apaindegi-front/vite.config.js
+++ ile-apaindegi-front/vite.config.js
@@ -15,4 +15,16 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0', // Permite que Vite sea accesible fuera del contenedor
+    port: 5173,      // El puerto que mapeas en Docker
+    strictPort: true,
+    hmr: {
+      path: '/ws',      // Ruta que configuramos en Apache (ProxyPass /ws)
+      clientPort: 80,   // El navegador se comunica por el puerto 80 (Apache)
+    },
+    watch: {
+      usePolling: true, // Necesario para que detecte cambios dentro de Docker en Windows/AWS
+    },
+  },
 })
